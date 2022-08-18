c7e17898da07f920ed130e3e0fc81826fab0e770
Alexey <pap220979@mail.ru>
2022-08-18 23:13:49 +0500

    added ism programs HomeWork 4

--- HomeWorks/HomeWork4/Program.cs
+++ HomeWorks/HomeWork4/Program.cs
@@ -52,8 +52,8 @@ Console.Write($"The sum of the digits in a number {a} = {n}");
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-/*
-int[] Create(int size)
+
+int[] CreateArray(int size)
 {
     int[] arrayNum = new int[size];
     for (int i=0; i<size; i++)
@@ -72,8 +72,7 @@ void ShowArray(int[] array)
 }
 
 Console.Write("Input size for array: ");
-int b = Convert.ToInt32(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 
-int[] myArray = Create(b);
+int[] myArray = CreateArray(num);
 ShowArray(myArray);
-*/
